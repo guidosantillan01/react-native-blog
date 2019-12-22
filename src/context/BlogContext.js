90edf8f56f6c0790a6a6b23f44b3441a47fe15fa
Guido Santillan <guidosantillan01@gmail.com>
2019-12-22 13:41:40 +0100

    Saving a new Post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -7,7 +7,8 @@ const blogReducer = (state, action) => {
         ...state,
         {
           id: Math.floor(Math.random() * 99999),
-          title: `Blog Post #${state.length + 1}`
+          title: action.payload.title,
+          content: action.payload.content
         }
       ];
     case 'DELETE_BLOGPOST':
@@ -18,8 +19,8 @@ const blogReducer = (state, action) => {
 };
 
 const addBlogPost = dispatch => {
-  return () => {
-    dispatch({ type: 'ADD_BLOGPOST' });
+  return (title, content) => {
+    dispatch({ type: 'ADD_BLOGPOST', payload: { title, content } });
   };
 };
 
--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -7,6 +7,8 @@ const CreateScreen = ({ navigation }) => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
 
+  const { addBlogPost } = useContext(Context);
+
   return (
     <View>
       <Text style={styles.label}>Enter Title:</Text>
@@ -21,7 +23,10 @@ const CreateScreen = ({ navigation }) => {
         value={content}
         onChangeText={text => setContent(text)}
       />
-      <Button title="Add Blog Post" />
+      <Button
+        title="Add Blog Post"
+        onPress={() => addBlogPost(title, content)}
+      />
     </View>
   );
 };
