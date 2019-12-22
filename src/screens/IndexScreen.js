4abfd3f7bd96ce8cd8e5c9f5abe9a40134ae8d00
Guido Santillan <guidosantillan01@gmail.com>
2019-12-22 12:56:38 +0100

    Add some stylgin

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 import { Context } from '../context/BlogContext';
 // Two values of Context, we need to do:
@@ -10,19 +11,38 @@ const IndexScreen = () => {
 
   return (
     <View>
-      <Text>Index Screen</Text>
       <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Feather style={styles.icon} name="trash" />
+            </View>
+          );
         }}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderColor: 'gray',
+    paddingHorizontal: 15
+  },
+  title: {
+    fontSize: 18
+  },
+  icon: {
+    fontSize: 24
+  }
+});
 
 export default IndexScreen;
