271d3ae7cca4ea8a06c50e5a652631cc8fcf786a
Guido Santillan <guidosantillan01@gmail.com>
2019-12-22 19:30:26 +0100

    Creating Posts with POSTS requests

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -34,8 +34,12 @@ const getBlogPosts = dispatch => {
 };
 
 const addBlogPost = dispatch => {
-  return (title, content, callback) => {
-    dispatch({ type: 'ADD_BLOGPOST', payload: { title, content } });
+  return async (title, content, callback) => {
+    await jsonServer.post('/blogposts', {
+      title,
+      content
+    });
+    // dispatch({ type: 'ADD_BLOGPOST', payload: { title, content } });
     if (callback) callback();
   };
 };
